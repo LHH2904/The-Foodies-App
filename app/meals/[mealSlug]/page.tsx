ad758381744af74699ce9cee1a75a2de8ff1663a
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-06-12 13:39:14 +0700

    remake shareMeal,mealSlug Page

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,60 @@
-export default function MealsDetailsPage() {
-    return <h1>Meals Details</h1>;
+import Image from "next/image";
+import {getMeal, Meal} from "@/lib/meals";
+import {notFound} from "next/navigation";
+
+interface MealsDetailsPageProps {
+    params: {
+        mealSlug: string;
+    };
+}
+
+export default function MealsDetailsPage({params}: MealsDetailsPageProps) {
+    const meal: Meal | undefined = getMeal(params.mealSlug)
+
+    if (!meal) {
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+    return (
+        <>
+            <header
+                className='flex px-4 py-8 gap-12 mx-auto max-w-[80rem]'
+            >
+                <div className='relative w-[30rem] h-[20rem]'>
+                    <Image
+                        src={meal.image}
+                        alt={meal.title}
+                        fill
+                        className='object-cover rounded-md shadow-[0_0_0.5rem_rgba(0,0,0,0.5)] animate-fade-slide-in-left'
+                    />
+                </div>
+                <div className='mt-2 px-4 text-[#ddd6cb] max-w-[40rem] animate-fade-slide-in-right'>
+                    <h1
+                        className=' m-0 text-[4.5rem] uppercase font-montserrat [text-shadow:0_0_0.5rem_rgba(0,0,0,0.5)]'
+                    >
+                        {meal.title}
+                    </h1>
+                    <p className='text-xl italic text-[#cfa69b]'>
+                        by <a
+                        href={`mailto:${meal.creator_email}`}
+                        className="bg-gradient-to-r from-[#f9572a] to-[#ff8a05] bg-clip-text text-transparent hover:[text-shadow:0_0_18px_rgba(248,190,42,0.8)]"
+                    >{meal.creator}</a>
+                    </p>
+                    <p className='text-xl'>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions
+                    }}
+                    className='text-lg leading-relaxed bg-[#6e6464] text-[#13120f] rounded-lg shadow-[0_0_0.5rem_rgba(0,0,0,0.5)] p-8 max-w-5xl my-8 mx-auto animate-fade-slide-in-from-bottom'>
+                </p>
+            </main>
+        </>
+    );
 }
--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,3 +1,105 @@
+'use client';
+import ImagePicker from "@/components/meals/image-picker";
+import {shareMeal} from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
+
 export default function ShareMealsPage() {
-    return <h1>Share Meals</h1>;
+
+    return (
+        <>
+            <header className='gap-12 my-12 mb-20 mx-auto w-[90%] max-w-[75rem] text-[#ddd6cb] text-[1.5rem]'>
+                <h1>
+                    Share your <span
+                    className='bg-gradient-to-r from-[#f9572a] to-[#ff8a05] bg-clip-text text-transparent'>favorite meal</span>
+                </h1>
+                <p>Or any other meal you feel needs sharing!</p>
+            </header>
+            <main className='w-[90%] max-w-[75rem] my-12 mx-auto text-white'>
+                <form className='max-w-[50rem]' action={shareMeal}>
+                    <div className='flex gap-4'>
+                        <p className='w-[100%]'>
+                            <label
+                                htmlFor="name"
+                                className='block mb-2 text-base font-bold uppercase text-[#b3aea5] font-montserrat'
+                            >
+                                Your name
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                required
+                                className='block w-full px-4 py-2 rounded border border-[#454952] bg-[#1c2027] text-[1.25rem] text-[#ddd6cb] font-montserrat'
+                            />
+                        </p>
+                        <p>
+                            <label
+                                htmlFor="email"
+                                className='block mb-2 text-base font-bold uppercase text-[#b3aea5] font-montserrat'
+                            >
+                                Your email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                required
+                                className='block w-full px-4 py-2 rounded border border-[#454952] bg-[#1c2027] text-[1.25rem] text-[#ddd6cb] font-montserrat'
+                            />
+                        </p>
+                    </div>
+                    <p>
+                        <label
+                            htmlFor="title"
+                            className='mt-2 block mb-2 text-base font-bold uppercase text-[#b3aea5] font-montserrat'
+                        >
+                            Title
+                        </label>
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            required
+                            className='block w-full px-4 py-2 rounded border border-[#454952] bg-[#1c2027] text-[1.25rem] text-[#ddd6cb] font-montserrat'
+                        />
+                    </p>
+                    <p>
+                        <label
+                            htmlFor="summary"
+                            className='mt-2 block mb-2 text-base font-bold uppercase text-[#b3aea5] font-montserrat'
+                        >
+                            Short Summary
+                        </label>
+                        <input
+                            type="text"
+                            id="summary"
+                            name="summary"
+                            required
+                            className='block w-full px-4 py-2 rounded border border-[#454952] bg-[#1c2027] text-[1.25rem] text-[#ddd6cb] font-montserrat'
+                        />
+                    </p>
+                    <p>
+                        <label
+                            htmlFor="instructions"
+                            className='mt-2 block mb-2 text-base font-bold uppercase text-[#b3aea5] font-montserrat'
+                        >
+                            Instructions
+                        </label>
+                        <textarea
+                            id="instructions"
+                            name="instructions"
+                            rows={10}
+                            required
+                            className='block w-full px-4 py-2 rounded border border-[#454952] bg-[#1c2027] text-[1.25rem] text-[#ddd6cb] font-montserrat'
+                        ></textarea>
+                    </p>
+                    <ImagePicker name='image' label='Your image'/>
+                    <p className='text-right'>
+                        <MealsFormSubmit/>
+                    </p>
+                </form>
+            </main>
+        </>
+    );
 }
